05d068f686c9caef0c048f44fcf02695c14f8a8b
Kai Prott <kai.prott@hotmail.de>
2022-12-25 22:34:39 +0100

    Implement day 25

--- src/Day25.hs
+++ src/Day25.hs
@@ -1,4 +1,44 @@
+{-# LANGUAGE DerivingStrategies #-}
 module Day25 where
 
+newtype SNAFU = SNAFU Int
+  deriving newtype (Eq, Ord, Bounded, Num, Enum, Real, Integral)
+
+instance Read SNAFU where
+    readsPrec :: Int -> ReadS SNAFU
+    readsPrec _ str =
+        let (snafuStr, rest) = span isSnafuChar str
+        in [(SNAFU (stringToSNAFU $ reverse snafuStr), rest)]
+
+isSnafuChar :: Char -> Bool
+isSnafuChar c = c `elem` "210-="
+
+stringToSNAFU :: String -> Int
+stringToSNAFU = sum . zipWith charToSnafu [(0 :: Int) ..]
+  where
+    charToSnafu expo c = 5 ^ expo * case c of
+        '2' -> 2
+        '1' -> 1
+        '0' -> 0
+        '-' -> -1
+        '=' -> -2
+        _   -> error "Invalid SNAFU character"
+
+instance Show SNAFU where
+    show :: SNAFU -> String
+    show (SNAFU 0) = "0"
+    show (SNAFU s) = intToSNAFU s []
+      where
+        intToSNAFU 0 = id
+        intToSNAFU n = case n `mod` 5 of
+            4 -> intToSNAFU ((n + 1) `div` 5) . ('-' :)
+            3 -> intToSNAFU ((n + 2) `div` 5) . ('=' :)
+            2 -> intToSNAFU ((n - 2) `div` 5) . ('2' :)
+            1 -> intToSNAFU ((n - 1) `div` 5) . ('1' :)
+            0 -> intToSNAFU ((n - 0) `div` 5) . ('0' :)
+            _ -> error "Invalid SNAFU number"
+
 day25 :: IO ()
-day25 = putStrLn "Day 25 not implemented yet"
+day25 = do
+    input <- map read . lines <$> readFile "input/day25"
+    print (sum input :: SNAFU)
